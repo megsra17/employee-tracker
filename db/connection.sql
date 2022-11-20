5da50a70d6b52c6752bea24030a09719763862ce
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-11-20 09:51:19 -0600

    first commit

---
+++ db/connection.sql
@@ -0,0 +1,9 @@
+const cTable = require('console.table');
+const mysql = require("mysql2");
+
+
+const connection = mysql.creatConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+});
